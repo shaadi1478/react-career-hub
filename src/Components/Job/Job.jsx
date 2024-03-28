73c079de56fbb2d997a7818440e5aaf6aa18cc1f
shaadi1478 <sheikhshaadi137@gmail.com>
2024-03-28 23:21:37 +0600

    added card data

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,3 +1,6 @@
+import { FaLocationDot } from "react-icons/fa6";
+import { AiOutlineDollar } from "react-icons/ai";
+
 const Job = ({ job }) => {
   const { logo, job_title, company_name, remote_or_onsite, job_type,  location, salary } = job;
   return (
@@ -11,8 +14,12 @@ const Job = ({ job }) => {
             <button className="px-5 rounded py-2 text-[#7E90FE] font-bold border border-[#7E90FE] mr-5">{remote_or_onsite}</button>
             <button className="px-5 py-2 text-[#7E90FE] font-bold border border-[#7E90FE] ">{job_type}</button>
         </div>
+        <div className="flex gap-8">
+            <h2 className="flex items-center gap-2"><FaLocationDot></FaLocationDot>{location}</h2>
+            <h2 className="flex items-center gap-2"><AiOutlineDollar></AiOutlineDollar>{salary}</h2>
+        </div>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">View Details</button>
         </div>
       </div>
     </div>
